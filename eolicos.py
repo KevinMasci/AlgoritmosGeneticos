130db894219820a15cfa18c831b2c1c04ec54606
Guillermo Gonzalez <102679682+frostaco@users.noreply.github.com>
2022-08-30 15:37:40 -0300

    Update eolicos.py
    Me tira error de indentacion en linea 17

--- eolicos.py
+++ eolicos.py
@@ -6,13 +6,16 @@ largoCromosoma = 10
 aerogenMax = 25
 crossover = 0.85
 mutacion = 0.05
+poblacion = []
 
 
 def parquenuevo():
-    x = 0
-    arrayvacio = []
     arrayvacio = np.zeros(anchoCromosoma, largoCromosoma)
     for x in range(0, aerogenMax):
-        i = randint(0,1)
-        if i== 1:
-
+        i = randint(0, 1)
+        if i == 1:
+            posicion1 = randint(0, largoCromosoma)
+            posicion2 = randint(0, anchoCromosoma)
+            if arrayvacio[posicion1][posicion2] == 0:
+                arrayvacio[posicion1][posicion2] = 1
+    poblacion.append(arrayvacio)
